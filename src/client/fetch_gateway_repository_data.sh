bdaeffd3414e86107e2a3a003536e6b7b7e8037b
mjambresic <matija.jambresic@yahoo.com>
2025-12-30 12:22:43 +0100

    Better data visibility on fetch

--- src/client/fetch_gateway_repository_data.sh
+++ src/client/fetch_gateway_repository_data.sh
@@ -20,7 +20,16 @@ git -C $LOCAL_REPO_PATH reset --hard origin/main
 
 # Check if the reset was successful
 if [ $? -eq 0 ]; then
-    printf "${GREEN}Data was successfully synced from the gateway repository.\n${RESET}"
+    printf "${GREEN}Data was successfully synced from the gateway repository.${RESET}\n"
+
+    # Connection Data
+    externalIp=$(get_stored_external_ip)
+    user=$(get_stored_user)
+    host=$(get_stored_host)
+
+    printf "External IP: ${YELLOW}%s${RESET}\n" "$externalIp"
+    printf "User: ${YELLOW}%s${RESET}\n" "$user"
+    printf "Host: ${YELLOW}%s${RESET}\n" "$host"
 else
-    printf "${RED}Failed to sync data from the gateway repository.\n${RESET}"
+    printf "${RED}Failed to sync data from the gateway repository.${RESET}\n"
 fi
